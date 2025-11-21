0299f06c74f39f379863cdc453563882a93dcc75
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-11-21 15:02:13 +0100

    Initialize project

---
+++ backend/src/main/java/com/nvt/eurosupply/EurosupplyApplication.java
@@ -0,0 +1,13 @@
+package com.nvt.eurosupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurosupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurosupplyApplication.class, args);
+	}
+
+}
