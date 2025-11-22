49408e68daf05afdca6dd89cd71144b720ea7233
Ana Šinik <sinikana7@gmail.com>
2025-11-22 12:40:38 +0100

    feat: create admin account on app startup

---
+++ backend/src/main/java/com/nvt/eurosupply/user/mappers/UserMapper.java
@@ -0,0 +1,11 @@
+package com.nvt.eurosupply.user.mappers;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserMapper {
+    private final ModelMapper modelMapper;
+}
