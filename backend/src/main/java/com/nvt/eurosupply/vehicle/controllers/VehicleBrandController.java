5abbcf17441d813f5f190b23224e7086753986ba
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-11-23 22:45:52 +0100

    docs: Add vehicle brand and model swagger documentation

--- backend/src/main/java/com/nvt/eurosupply/vehicle/controllers/VehicleBrandController.java
+++ backend/src/main/java/com/nvt/eurosupply/vehicle/controllers/VehicleBrandController.java
@@ -3,6 +3,9 @@ package com.nvt.eurosupply.vehicle.controllers;
 import com.nvt.eurosupply.vehicle.dtos.VehicleBrandDto;
 import com.nvt.eurosupply.vehicle.dtos.VehicleModelDto;
 import com.nvt.eurosupply.vehicle.services.VehicleBrandService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +16,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/vehicles/brands")
 @RequiredArgsConstructor
-@Tag(name = "Vehicle Brands", description = "Vehicle brand management API")
+@Tag(name = "Vehicle Brands and Models", description = "Vehicle brand and model management API")
 @CrossOrigin
 public class VehicleBrandController {
 
     private final VehicleBrandService service;
 
+    @Operation(
+            summary = "Get all brands",
+            description = "Fetches a list of all brands in the system."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of brands"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping
     public ResponseEntity<List<VehicleBrandDto>> getBrands() {
         return ResponseEntity.ok(service.getBrands());
     }
 
+    @Operation(
+            summary = "Get all models of a brand",
+            description = "Returns all models that belong to the given brand ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Models retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Brand not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/{id}/models")
     public ResponseEntity<List<VehicleModelDto>> getVehicleBrandModels(@PathVariable Long id) {
         return ResponseEntity.ok(service.getBrandModels(id));
     }
-
 
 }
