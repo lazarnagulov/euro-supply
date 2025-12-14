e43a1738db2f91890cbbed0e0021f554b56f1c64
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-12-14 10:47:39 +0100

    bugfix: Missing constraints in update vehicle dto

--- backend/src/main/java/com/nvt/eurosupply/vehicle/dtos/UpdateVehicleRequestDto.java
+++ backend/src/main/java/com/nvt/eurosupply/vehicle/dtos/UpdateVehicleRequestDto.java
@@ -1,5 +1,7 @@
 package com.nvt.eurosupply.vehicle.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UpdateVehicleRequestDto {
+
+    @NotBlank(message = "Registration Number is required")
     private String registrationNumber;
+
+    @NotNull(message = "Max Load in kg is required")
     private Double maxLoadKg;
+
+    @NotNull(message = "Brand is required")
     private Long brandId;
+
+    @NotNull(message = "Model is required")
     private Long modelId;
 }
